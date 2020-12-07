4f39a1d2c8a583c21ea73b35d86a3265474321ea
evgen66rus <evgen.alyoshin@gmail.com>
2020-12-07 12:28:19 +0500

    fixed variables, added arguments

--- ec2Tags.py
+++ ec2Tags.py
@@ -2,10 +2,18 @@
 # to volumes and network interfaces attached to the instance.
 
 import boto3
+import sys
+
+try:
+    region = sys.argv[1]
+except IndexError:
+    print('Provide a region name as argument, e.g. us-east-1')
+    exit(1)
 
 # Edit parameters:
 tagKey = 'End User' #Tag key of an instance you need to copy to its volumes and network interfaces
-region = 'us-east-1'
+#region = input('Input region name (example: us-east-1): ')
+
 
 # Do not edit below
 ec2 = boto3.resource('ec2', region_name=region).instances.all()
@@ -16,25 +24,25 @@ instance_id = ''
 
 for group in response:
     instances = group['Instances']
-    for id in instances:
-        instance_id = id['InstanceId']
+    for id_list in instances:
+        instance_id = id_list['InstanceId']
         #print(instance_id)
 
         # finding value of the tag key
-        tags = id['Tags']
-        for t in tags:
-            q = t['Key']
-            j = t['Value']
-            if q == tagKey:
-                tag = j
+        tags = id_list['Tags']
+        for tag_values in tags:
+            key_value = tag_values['Key']
+            value_value = tag_values['Value']
+            if key_value == tagKey:
+                tag = value_value
         #print(tag)
 
         # copying tag key and value to volumes
-        volumes = id['BlockDeviceMappings']
-        for values in volumes:
-            ebs = values['Ebs']
+        volumes = id_list['BlockDeviceMappings']
+        for v_values in volumes:
+            ebs = v_values['Ebs']
             volume_id = ebs['VolumeId']
-            kek = boto3.resource('ec2', region_name=region).Volume(volume_id).create_tags(Tags=[
+            set_tags = boto3.resource('ec2', region_name=region).Volume(volume_id).create_tags(Tags=[
                 {
                     'Key': tagKey,
                     'Value': tag
@@ -43,10 +51,10 @@ for group in response:
             #print(volume_id)
 
         # copying tag key and value to network interfaces
-        networks = id['NetworkInterfaces']
+        networks = id_list['NetworkInterfaces']
         for interfaces in networks:
             eni_id = interfaces['NetworkInterfaceId']
-            kek = boto3.resource('ec2', region_name=region).Volume(eni_id).create_tags(Tags=[
+            set_tags = boto3.resource('ec2', region_name=region).Volume(eni_id).create_tags(Tags=[
                 {
                     'Key': tagKey,
                     'Value': tag
