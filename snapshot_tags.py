59527a767f74f50df411943c1c15107560cadb49
evgen66rus <evgen.alyoshin@gmail.com>
2020-12-10 00:01:30 +0500

    added some functions

--- snapshot_tags.py
+++ snapshot_tags.py
@@ -1,42 +1,64 @@
 # This script copies tag value of specified tag key of ec2 volume
-# to snapshots made from this volume.
+# to snapshots made from the volume.
 
 import boto3
 
+tagKey = input("Input tag key: ")
+account_id = input("Input account ID: ")
+region = input("Input a region (e.g. us-east-1): ")
 
-# Edit parameters:
-tagKey = ''            # Tag key of an instance you need to copy to its volumes and network interfaces
-account_id = ''        # Check it on AWS Support page
-region = ''            # Specify a region
+def for_region():
+    '''
+    Copies tag value of tag key for specified region in the account.
+    :return:
+    '''
 
+    request_get = boto3.client('ec2', region_name=region)
 
-# Do not edit below
-request_get = boto3.client('ec2',region_name=region)
-filters = [{'Name':'tag-key','Values':[tagKey]}]
-request_snapshot = request_get.describe_snapshots(Filters=[{'Name':'owner-id','Values':[account_id]}])
-request_volumes = request_get.describe_volumes(Filters=[{'Name':'tag-key','Values':[tagKey]}])
+    def get_snapshot():
+        '''
+        Returns snapshots for the account in the region specified in get_region() function.
+        :return: List of snapshots
+        '''
+        request_snapshot = request_get.describe_snapshots(Filters=[{'Name':'owner-id','Values':[account_id]}])
+        return request_snapshot
+    get_snapshot()
 
+    def get_volume():
+        '''
+        Returns volumes in the region with specified tag key.
+        :return: List of volumes
+        '''
+        request_volumes = request_get.describe_volumes(Filters=[{'Name':'tag-key','Values':[tagKey]}])
+        return request_volumes
+    get_volume()
 
+    def tagging_snapshots():
+        '''
+        Tagging snapshots by volume ID with tag value of the volume.
+        :return: None
+        '''
 
-for response_snapshot in request_snapshot:
-    for snapshots in request_snapshot['Snapshots']:
-        volume_to_tag = snapshots['VolumeId']
-        snapshot_id = snapshots['SnapshotId']
-        for response_volumes in request_volumes:
-            for volumes in request_volumes['Volumes']:
-                volume_id_resp = volumes['VolumeId']
-                volume_tags = volumes['Tags']
-                for volume_keys in volume_tags:
-                    if volume_keys['Key'] == tagKey:
-                        tag = volume_keys['Value']
-                        volume_id = volume_id_resp
-                        if volume_id == volume_to_tag:
-                            set_snap_tag = boto3.resource('ec2', region_name=region).Snapshot(snapshot_id).create_tags(Tags=[
-                                {
-                                    'Key': tagKey,
-                                    'Value': tag
-                                }
-                            ])
+        for response_snapshot in get_snapshot():
+            for snapshots in get_snapshot()['Snapshots']:
+                volume_to_tag = snapshots['VolumeId']
+                snapshot_id = snapshots['SnapshotId']
+                for response_volumes in get_volume():
+                    for volumes in get_volume()['Volumes']:
+                        volume_id_resp = volumes['VolumeId']
+                        volume_tags = volumes['Tags']
+                        for volume_keys in volume_tags:
+                            if volume_keys['Key'] == tagKey:
+                                tag = volume_keys['Value']
+                                volume_id = volume_id_resp
+                                if volume_id == volume_to_tag:
+                                    print(tagKey)
+                                    set_snap_tag = boto3.resource('ec2', region_name=region).Snapshot(snapshot_id).create_tags(Tags=[
+                                        {
+                                            'Key': tagKey,
+                                            'Value': tag
+                                        }
+                                    ])
 
-print("Done!")
-
+    tagging_snapshots()
+for_region()
