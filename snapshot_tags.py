9e8a32c4b18505c6eb085889802f99d043bf2e52
evgen66rus <evgen.alyoshin@gmail.com>
2020-12-14 20:44:45 +0500

    something added

--- snapshot_tags.py
+++ snapshot_tags.py
@@ -4,39 +4,53 @@
 import boto3
 from botocore.exceptions import ClientError
 
-tagKey = input("Input tag key: ")
-account_id = input("Input account ID: ")
-region = input("Input a region (e.g. us-east-1): ")
+tagKey = 'End User'
+account_id = '543033905888'
+region = 'us-east-1'
+#tagKey = input("Input tag key: ")
+#account_id = input("Input account ID: ")
+#region = input("Input a region (e.g. us-east-1): ")
 request_get = boto3.client('ec2', region_name=region)
 
+def get_volumes():
+    '''
+    Returns volumes ID and tag value in the region with specified tag key.
+    :return: List of volumes
+    '''
+    print("Getting volumes.....")
+    request_volumes = request_get.describe_volumes(Filters=[{'Name':'tag-key','Values':[tagKey]}])
+    for all_volumes in request_volumes['Volumes']:
+        volume_id = all_volumes['VolumeId']
+        volume_tags = all_volumes['Tags']
+        for tag_key in volume_tags:
+            if tag_key['Key'] == tagKey:
+                volume_id = volume_id
+                volume_tag_value = tag_key['Value']
+    return volume_id, volume_tag_value
+get_volumes()
 
-def get_snapshots():
+def get_snapshots(volume_id):
     '''
     Returns snapshots for the account in the region.
     :return: List of snapshots
     '''
     print("Getting snapshots....")
-    request_snapshots = request_get.describe_snapshots(Filters=[{'Name':'owner-id','Values':[account_id]}])
+    request_snapshots = request_get.describe_snapshots(Filters=[
+        {'Name':'owner-id','Values':[account_id]},
+        {'Name':'volume-id','Values':volume_id}
+    ])
+    print(request_snapshots)
     return request_snapshots
 
-
-def get_volumes():
-    '''
-    Returns volumes in the region with specified tag key.
-    :return: List of volumes
-    '''
-    print("Getting volumes.....")
-    request_volumes = request_get.describe_volumes(Filters=[{'Name':'tag-key','Values':[tagKey]}])
-    return request_volumes
-
+get_snapshots()
 
 def tagging_snapshots():
     '''
     Copying tag value of the volume with specified tag key to snapshots.
     :return: None
     '''
-    all_snapshots = get_snapshots()
-    all_volumes = get_volumes()
+    #all_snapshots = get_snapshots()
+    #all_volumes = get_volumes()
     available_zones = [region + 'a', region + 'b', region + 'c', region + 'd']
     for snapshots in all_snapshots['Snapshots']:
         volume_id_in_snap = snapshots['VolumeId']
@@ -66,5 +80,5 @@ def tagging_snapshots():
                             except ClientError:
                                 pass
 
-tagging_snapshots()
+#tagging_snapshots()
 print("Done!")
